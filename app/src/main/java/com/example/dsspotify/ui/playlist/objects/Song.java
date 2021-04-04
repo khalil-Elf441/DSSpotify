6331a66db9fddb5fe7cf62b567887b537fb1cc8e
khalil-Elf441 <elfallahikhalil@gmail.com>
2021-04-05 01:50:34 +0200

    class song

--- app/src/main/java/com/example/dsspotify/ui/playlist/objects/Song.java
+++ app/src/main/java/com/example/dsspotify/ui/playlist/objects/Song.java
@@ -3,5 +3,45 @@ package com.example.dsspotify.ui.playlist.objects;
 public class Song {
 
 
-    
+    private String title;
+    private String artist;
+    private String filename;
+    private String image;
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
