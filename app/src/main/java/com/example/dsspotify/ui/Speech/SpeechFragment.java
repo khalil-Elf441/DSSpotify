c7064ea102be0c9ba37f924f3cbaa9e8e4f2f923
khalil-Elf441 <elfallahikhalil@gmail.com>
2021-04-05 23:30:24 +0200

    add Speach to Text using Google API (intent)

--- app/src/main/java/com/example/dsspotify/ui/Speech/SpeechFragment.java
+++ app/src/main/java/com/example/dsspotify/ui/Speech/SpeechFragment.java
@@ -1,10 +1,15 @@
 package com.example.dsspotify.ui.Speech;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,22 +19,53 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.dsspotify.R;
 
+import java.util.Locale;
+
 public class SpeechFragment extends Fragment {
 
     private SpeechViewModel speechViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        speechViewModel =
+
+
+
+
+        View root = inflater.inflate(R.layout.fragment_speech, container, false);
+
+        ImageView imgView_microphone = root.findViewById(R.id.imgView_microphone);
+
+        imgView_microphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                        "Speak something...");
+                try {
+                    startActivityForResult(intent, 666);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getContext(),
+                            "Sorry! Speech recognition is not supported in this device.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+         /*
+         speechViewModel =
                 new ViewModelProvider(this).get(SpeechViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+         final TextView textView = root.findViewById(R.id.text_home);
         speechViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+        */
+
         return root;
     }
 }
